619901a4e20f1295d53ca67987f131d44ce0d2e5
Bautista Priano <bautipriano@gmail.com>
2022-11-09 19:14:43 -0300

    Fin Gestion Promociones, Fixes en ControladorGestionPromociones

--- src/main/java/excepciones/PromocionNoExistenteException.java
+++ src/main/java/excepciones/PromocionNoExistenteException.java
@@ -1,7 +1,9 @@
 package excepciones;
 
+
 public class PromocionNoExistenteException extends Exception{
-    public PromocionNoExistenteException(){
-        super("La promocion ingresada no se encuentra cargada en el sistema");
+
+    public PromocionNoExistenteException(String msg){
+        super(msg);
     }
 }

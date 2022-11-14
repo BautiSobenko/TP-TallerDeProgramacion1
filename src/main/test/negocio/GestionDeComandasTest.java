8fcf44b46ba78e651e7f80bbf99c04fd1d567ca9
Bautista Priano <bautipriano@gmail.com>
2022-11-14 17:37:40 -0300

    nada

---
+++ src/main/test/negocio/GestionDeComandasTest.java
@@ -0,0 +1,28 @@
+package negocio;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GestionDeComandasTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void abrirComanda() {
+
+    }
+
+    @Test
+    public void cargarPedido() {
+    }
+}
